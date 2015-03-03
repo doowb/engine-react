e722e413de9bcad5ed77cab51c5a48fe7facf3d0
Brian Woodward <brian.woodward@gmail.com>
2015-03-02 19:47:54 -0500

    simplifying example

--- examples/assemblefile.js
+++ examples/assemblefile.js
@@ -21,46 +21,28 @@ var dirname = path.join.bind(path, __dirname);
 var data = {};
 var comments = require('./data/comments.json');
 data.comments = {
-  list: comments,
+  data: comments,
   pollInterval: 2000
 };
 
 data.users = {
-  list: comments,
+  data: comments,
   pollInterval: 3000
 };
 
 // add the engines for the specific extensions
 assemble.engine('.jsx', engine);
 
+// set default layout for handlebars templates
 assemble.option('layout', 'default');
 
-// create a new assemble type to use in handlebars assembles
-assemble.create('component', { isRenderable: true });
-assemble.asyncHelper('component', function (name, ctx, options, next) {
-    if (typeof next !== 'function') {
-      next = options;
-      options = ctx;
-      ctx = {};
-    }
-    var app = this.app;
-    var context = this.context;
-    var component = app.findRenderable(name, ['components']);
-    if (!component) {
-      console.log('can not find', name, 'component');
-      return next(null, '');
-    }
-    context.options = context.options || {};
-    context.options.layout = false;
-
-    context = extend({}, context, ctx, options.hash);
-    component.render(context, next);
-});
+// create a new template type to use in handlebars template
+assemble.create('component');
 
 // add handlebars layouts
 assemble.layouts(dirname('src/layouts/*.hbs'));
 
-// add react assembles used in the client-side app
+// add react templates used in the client-side app
 assemble.components(dirname('src/apps/*.jsx'));
 
 assemble.task('client', function (done) {
